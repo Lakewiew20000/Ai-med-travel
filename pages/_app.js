dcff02430bddc71bbf9efdf8bde5956ba6aef31c
Lakewiew20000 <juliegoesglobal43@gmail.com>
2025-06-17 17:38:27 -0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import '../styles/globals.css';
-import Navbar from '../components/Navbar';
+import '../styles/globals.css'
+import Navbar from '../components/Navbar'
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return (
     <>
       <Navbar />
       <Component {...pageProps} />
     </>
-  );
+  )
 }
+
+export default MyApp
 
