ffc461a30c0a08dcdfa88fe87516f9f0e87bb890
Lakewiew20000 <juliegoesglobal43@gmail.com>
2025-06-18 09:14:37 -0700

    Update contact.js

--- pages/contact.js
+++ pages/contact.js
@@ -1,4 +1,8 @@
-export default function ContactPage() {
-  return <div>Articles Page Coming Soon</div>;
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+      <p>We're here to help. Reach out with questions or to start your journey.</p>
+    </div>
+  );
 }
-
