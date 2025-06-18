898cb4c9ddd61163997e075ebaaaab6aa9ac7c52
Lakewiew20000 <juliegoesglobal43@gmail.com>
2025-06-18 05:29:30 -0700

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,12 +3,12 @@ import Link from 'next/link'
 const Navbar = () => {
   return (
     <nav>
-      <div><Link href="/">AI Med Travel</Link></div>
       <div>
-        <Link href="/search">Search</Link>
+        <Link href="/">Home</Link>
+        <Link href="/articles">Articles</Link>
         <Link href="/innovation">Innovation</Link>
         <Link href="/providers">Providers</Link>
-        <Link href="/articles">Articles</Link>
+        <Link href="/search">Search</Link>
         <Link href="/contact">Contact</Link>
         <Link href="/login">Login</Link>
       </div>
@@ -17,3 +17,4 @@ const Navbar = () => {
 }
 
 export default Navbar
+
