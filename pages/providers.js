cee31fe2089df661227190d63e0a4386274aa951
Lakewiew20000 <juliegoesglobal43@gmail.com>
2025-06-18 06:30:57 -0700

    Update providers.js

--- pages/providers.js
+++ pages/providers.js
@@ -1,4 +1,8 @@
-export default function ProviderPage() {
-  return <div>Articles Page Coming Soon</div>;
+export default function Providers() {
+  return (
+    <div>
+      <h1>Find Trusted Providers</h1>
+      <p>Browse our network of top-rated hospitals, clinics, and specialists worldwide.</p>
+    </div>
+  );
 }
-
