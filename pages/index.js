792b82f12b80c4a765ca0b8bc7d443d69f0e5c1e
Lakewiew20000 <juliegoesglobal43@gmail.com>
2025-06-19 21:21:11 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import Image from 'next/image';
 import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
-import TopThreeMatches from "@/components/TopThreeMatches"; // ✅ import here
+import TopThreeMatches from "../components/TopThreeMatches";
+
 
 export default function Home() {
   return (
